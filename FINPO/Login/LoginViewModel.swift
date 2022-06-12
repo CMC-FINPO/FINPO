ecea5a41565f931aa59259a6c895de84099fa55c
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-06-13 01:11:49 +0900

    추가 정보 입력 전 User 정보 저장 및 방출 VM 추가

--- FINPO/Login/LoginViewModel.swift
+++ FINPO/Login/LoginViewModel.swift
@@ -45,6 +45,7 @@ class LoginViewModel {
         let subRegionTapped = PublishRelay<Int>()
         let forUserInterestObserver = PublishRelay<Int>() //유저의 상위 카테고리 저장 (탭 될때)
         let interestButtonTapped = PublishRelay<Void>()
+        let semiSignupConfirmButtonTapped = PublishRelay<Void>()
     }
     
     struct OUTPUT {
@@ -62,6 +63,9 @@ class LoginViewModel {
         var forUserInterestOutput = PublishRelay<[Int]>()
 //        var interestButtonValid = PublishRelay<Bool>()
         var interestButtonValid: Driver<Bool> = PublishRelay<Bool>().asDriver(onErrorJustReturn: false)
+//        var isSemiSignupComplete = PublishRelay<Bool>()
+        var isSemiSignupComplete = PublishRelay<User>()
+        
         var errorValue = PublishRelay<Error>()
     }
     
@@ -93,7 +97,7 @@ class LoginViewModel {
         //check nickname validation
         input.nickNameObserver
             .observe(on: MainScheduler.asyncInstance)
-            .flatMap { _ in self.checkNicknameValid() }
+            .flatMap { _ in self.checkNicknameValid() } //중복:true
             .subscribe({ valid in
                 switch valid {
                 case .next(let valid):
@@ -120,9 +124,9 @@ class LoginViewModel {
             }
         }).disposed(by: disposeBag)
         
-        input.emailObserver.subscribe(onNext: { email in
-            self.user.email = email
-        }).disposed(by: disposeBag)
+//        input.emailObserver.subscribe(onNext: { email in
+//            self.user.email = email
+//        }).disposed(by: disposeBag)
         
         input.forUserInterestObserver
             .subscribe(onNext: { id in
@@ -133,44 +137,34 @@ class LoginViewModel {
                     print("중복 전 \(self.user.category)")
                 }
             }).disposed(by: disposeBag)
-        
-//        input.interestButtonTapped
-//            .subscribe(onNext: {
-//                if(self.user.category.count > 0) {
-//                    print("asdkjalksdjl")
-//                    self.output.interestButtonValid.accept(true)
-//                } else {
-//                    print("펄스펄스")
-//                    self.output.interestButtonValid.accept(false)
-//                }
-//            }).disposed(by: disposeBag)
+
                         
         ///OUTPUT
         output.genderValid = input.genderObserver.asDriver(onErrorJustReturn: .none)
         
-        input.emailObserver
-            .flatMap { self.checkEmail(email: $0)}
-            .observe(on: MainScheduler.asyncInstance)
-            .subscribe({ valid in
-                switch valid {
-                case .next(_ ):
-                    self.checkEmailValid().subscribe(onNext: { v in
-                        self.output.isEmailValid.accept(v)
-                    }).disposed(by: self.disposeBag)
-                case .completed:
-                    break
-                case .error(let error):
-                    self.output.errorValue.accept(error)
-                }
-            }).disposed(by: disposeBag)
+//        input.emailObserver
+//            .flatMap { self.checkEmail(email: $0)}
+//            .observe(on: MainScheduler.asyncInstance)
+//            .subscribe({ valid in
+//                switch valid {
+//                case .next(_ ):
+//                    self.checkEmailValid().subscribe(onNext: { v in
+//                        self.output.isEmailValid.accept(v)
+//                    }).disposed(by: self.disposeBag)
+//                case .completed:
+//                    break
+//                case .error(let error):
+//                    self.output.errorValue.accept(error)
+//                }
+//            }).disposed(by: disposeBag)
                 
-        output.buttonValid = Driver.combineLatest(input.nameObserver.asDriver(onErrorJustReturn: ""),
+        output.buttonValid = Driver.combineLatest(input.nameObserver.asDriver(onErrorRecover: { _ in return .never()}),
                                                   output.isNicknameValid.asDriver(onErrorJustReturn: false),
-                                                  input.birthObserver.asDriver(onErrorJustReturn: ""),
+                                                  input.birthObserver.asDriver(onErrorJustReturn: "errorTest"),
                                                   input.genderObserver.asDriver(onErrorJustReturn: .none),
-                                                  output.isEmailValid.asDriver(onErrorJustReturn: false),
-                                                  resultSelector: { (a, b, c, d, e) in
-        if a != "" && b != false  && c != "" && d != .none && e != false {
+                                                  resultSelector: { (a, b, c, d) in
+        if a != "" && b != true  && c != "" && d != .none {
+                print("버튼 색 변경조건 완료 -> true 방출")
                 return true
             }
             return false
@@ -212,6 +206,19 @@ class LoginViewModel {
             }
             .asDriver(onErrorJustReturn: false)
    
+        input.semiSignupConfirmButtonTapped
+            .flatMap { _ in self.semiSignup() }
+            .subscribe( { valid in
+                switch valid {
+                case .next(let tr):
+                    self.output.isSemiSignupComplete.accept(tr)
+                case .error(let err):
+                    self.output.errorValue.accept(err)
+                case .completed:
+                    break
+                }
+            }).disposed(by: disposeBag)
+        
         
     }
     
@@ -222,7 +229,18 @@ class LoginViewModel {
                 UserApi.shared.rx.loginWithKakaoTalk()
                     .subscribe(onNext: { (oauthToken) in
                         print("loginWithKakaoTalk() success.")
-                        observer.onNext(true)
+                        UserApi.shared.me { user, error in
+                            if let error = error {
+                                self.output.errorValue.accept(error)
+                            } else { ///회원정보 가져오기 성공 시
+                                self.input.nickNameObserver.accept(user?.kakaoAccount?.profile?.nickname ?? "")
+                                self.user.profileImg = user?.kakaoAccount?.profile?.profileImageUrl
+                                observer.onNext(true)
+                            }
+                        }
+                        self.user.accessTokenFromKAKAO = oauthToken.accessToken
+                    
+                        print("카카오 엑세스 토큰 from kakao api server: \(self.user.accessTokenFromKAKAO)")
                     }, onError: { (error) in
                         print("error occured: \(error)")
                         observer.onError(error)
@@ -255,11 +273,11 @@ class LoginViewModel {
                                 let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
                                 let result = json?["data"] as? Bool ?? false
                                 if result {
-                                    print("중복된 이메일")
-                                    observer.onNext(false)
-                                } else {
-                                    print("중복되지 않은 이메일")
+                                    print("중복된 닉네임")
                                     observer.onNext(true)
+                                } else {
+                                    print("중복되지 않은 닉네임")
+                                    observer.onNext(false)
                                 }
                             }
                         }
@@ -272,61 +290,61 @@ class LoginViewModel {
         }
     }
     
-    private func checkEmail(email: String) -> Observable<Bool> {
-        return Observable.create { observer in
-            if (!email.isEmpty && email.contains("@") && email.contains(".")) {
-                observer.onNext(true)
-            }
-            else {
-                observer.onCompleted()
-            }
-            
-            return Disposables.create()
-        }
-    }
+//    private func checkEmail(email: String) -> Observable<Bool> {
+//        return Observable.create { observer in
+//            if (!email.isEmpty && email.contains("@") && email.contains(".")) {
+//                observer.onNext(true)
+//            }
+//            else {
+//                observer.onCompleted()
+//            }
+//
+//            return Disposables.create()
+//        }
+//    }
     
-    private func checkEmailValid() -> Observable<Bool> {
-        return Observable.create { observer in
-            
-            let url = "https://dev.finpo.kr/user/check-duplicate?email=\(self.user.email)"
-  
-            let parameter: Parameters = [
-                "email": self.user.email
-            ]
-            let header: HTTPHeaders = [
-                "Content-Type": "application/json;charset=UTF-8"
-            ]
-            
-            AF.request(url, method: .get, parameters: parameter, encoding: URLEncoding.default, headers: header)
-                .validate(statusCode: 200..<300)
-                .response { response in
-                    switch response.result {
-                    case .success(let data):
-                        if let data = data {
-                            do {
-                                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-                                let result = json?["data"] as? Bool ?? false
-                                print(self.user.email)
-                                print("email check api 성공, 이메일 겹침여부 \(result)!")
-                                if result == true { //중복일 때
-                                    observer.onNext(true)
-                                    
-                                } else {
-                                    observer.onNext(false) //중복 아닐 때
-//                                    observer.onNext(true) //MARK: TEST 중복 아닐 때
-                                }
-                                
-                            }
-                        }
-                    case .failure(let error):
-                        print("email Valite 에러발생!!!: \(error)")
-                        observer.onError(error)
-                    }
-                }
-            return Disposables.create()
-        }
-
-    }
+//    private func checkEmailValid() -> Observable<Bool> {
+//        return Observable.create { observer in
+//
+//            let url = "https://dev.finpo.kr/user/check-duplicate?email=\(self.user.email)"
+//
+//            let parameter: Parameters = [
+//                "email": self.user.email
+//            ]
+//            let header: HTTPHeaders = [
+//                "Content-Type": "application/json;charset=UTF-8"
+//            ]
+//
+//            AF.request(url, method: .get, parameters: parameter, encoding: URLEncoding.default, headers: header)
+//                .validate(statusCode: 200..<300)
+//                .response { response in
+//                    switch response.result {
+//                    case .success(let data):
+//                        if let data = data {
+//                            do {
+//                                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+//                                let result = json?["data"] as? Bool ?? false
+//                                print(self.user.email)
+//                                print("email check api 성공, 이메일 겹침여부 \(result)!")
+//                                if result == true { //중복일 때
+//                                    observer.onNext(true)
+//
+//                                } else {
+//                                    observer.onNext(false) //중복 아닐 때
+////                                    observer.onNext(true) //MARK: TEST 중복 아닐 때
+//                                }
+//
+//                            }
+//                        }
+//                    case .failure(let error):
+//                        print("email Valite 에러발생!!!: \(error)")
+//                        observer.onError(error)
+//                    }
+//                }
+//            return Disposables.create()
+//        }
+//
+//    }
     
     func getMainRegionDataToTableView() {
         let url = "https://dev.finpo.kr/region/name"
@@ -419,10 +437,59 @@ class LoginViewModel {
         }
     }
     
+    func semiSignup() -> Observable<User> {
+        return Observable.create { observer in
+            let url = "https://dev.finpo.kr/oauth/register/kakao"
+            let parameter = self.user.toDic()
+            let header: HTTPHeaders = [
+                "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8;boundary=6o2knFse3p53ty9dmcQvWAIx1zInP11uCfbm",
+                "Authorization":"Bearer ".appending((self.user.accessTokenFromKAKAO))
+            ]
+            
+            AF.upload(multipartFormData: { (multipart) in
+                for (key, value) in parameter {
+                    multipart.append("\(value)".data(using: .utf8, allowLossyConversion: false)!, withName: "\(key)")
+                    print("multipart 데이터 추가 됨")
+                }
+            }, to: url, method: .post, headers: header).responseJSON {
+                (response) in
+                guard let statusCode = response.response?.statusCode else {
+                    print("status code is not valid")
+                    return
+                }
+                print("스태이터스 코드: \(statusCode)")
+                switch statusCode {
+                case 200..<299:
+                    print("회원정보 등록 진행 중(이게 보인다고 반드시 성공은 아니며, 중복인 경우가 포함)")
+                    do {
+                        let json = try response.result.get() as? [String: Any]
+                        let errorCode = json?["errorCode"] as? Int ?? 0
+                        if (errorCode == 20002) {
+                            print("이미 등록된 회원입니다.")
+                            observer.onError(viewModelError.alreadyExistAccount)
+                        } else {
+                            let jsonData = json?["data"] as? [String: Any]
+                            let accessToken = jsonData?["accessToken"] as? String
+                            let refreshToken = jsonData?["refreshToken"] as? String
+                            self.user.accessTokenFromKAKAO = accessToken ?? ""
+                            self.user.refreshToken = refreshToken ?? ""
+                            observer.onNext(self.user)
+                        }
+                    } catch { print("알 수 없는 에러 발생") }
+                    
+                default:
+                    print("실패!!: \(response.error)")
+                    observer.onError(viewModelError.alreadyExistElement)
+                }
+            }
+            return Disposables.create()
+        }
+    }
     
     
 }
 
 enum viewModelError: Error {
     case alreadyExistElement
+    case alreadyExistAccount
 }
