21325cb2732a65a7bfdde60cf4575068a447525e
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-06-14 02:34:41 +0900

    add imageview extension

--- FINPO/CommonExtension/UIKitExtension.swift
+++ FINPO/CommonExtension/UIKitExtension.swift
@@ -8,6 +8,27 @@
 import Foundation
 import UIKit
 
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
+
 extension UIColor {
     //rgb 컬러 정수로 계산하는 UIColor Extension
     static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
