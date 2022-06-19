9457c5fed3518acff8f5f62724a0c1f55a15ba06
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-06-20 01:15:35 +0900

    게시판 리유저블 셀 추가 및 셀 레이아웃 설정

---
+++ FINPO/MyPage/SegmentedControl/BoardTableViewCell.swift
@@ -0,0 +1,157 @@
+//
+//  BoardTableViewCell.swift
+//  FINPO
+//
+//  Created by 이동희 on 2022/06/19.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class BoardTableViewCell: UITableViewCell {
+
+    public var userImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "profile")
+        return imageView
+    }()
+    
+    public var userName: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = UIFont(name: "AppleSDGothicNeo-Semibold", size: 16)
+        label.textColor = UIColor(hexString: "000000")
+        label.text = "핀포닉네임"
+        return label
+    }()
+    
+    public var dateLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 12)
+        label.textColor = UIColor(hexString: "999999")
+        label.text = "dummy time 2022/06/20"
+        return label
+    }()
+    
+    public var contentLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.lineBreakMode = .byTruncatingTail
+        label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 18)
+        label.textColor = UIColor(hexString: "000000")
+        label.text = "청년저축계좌랑 청년희망키움통장이랑 다른 거 맞죠? 어떤 점이 다른 건가요? 제가 어디에 해당되는건가요? 알려주실 분 급구합니다"
+        return label
+    }()
+    
+    public var likeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "like"), for: .normal)
+        button.setImage(UIImage(named: "like"), for: .selected)
+        return button
+    }()
+    
+    public var bookMarkButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "scrap_inactive"), for: .normal)
+        button.setImage(UIImage(named: "scrap_active"), for: .selected)
+        return button
+    }()
+    
+    public var likeCountLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 12)
+        label.textColor = UIColor(hexString: "999999")
+        label.text = "좋아요 3"
+        return label
+    }()
+    
+    public var commentCountLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 12)
+        label.textColor = UIColor(hexString: "999999")
+        label.text = "댓글 1"
+        return label
+    }()
+    
+    public var viewsCountLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 12)
+        label.textColor = UIColor(hexString: "999999")
+        label.text = "조회수 30"
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.backgroundColor = UIColor(hexString: "EBEBEB") // hex 수정
+        [userImageView, userName, dateLabel, contentLabel, likeButton, bookMarkButton, likeCountLabel, commentCountLabel, viewsCountLabel].forEach {
+            contentView.addSubview($0)
+        }
+        
+        userImageView.layer.cornerRadius = userImageView.frame.width/2
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        userImageView.snp.makeConstraints {
+            $0.leading.equalTo(contentView.snp.leading).inset(10)
+            $0.top.equalTo(contentView.snp.top).inset(10)
+            $0.height.width.equalTo(35)
+        }
+        
+        userName.snp.makeConstraints {
+            $0.top.equalTo(userImageView.snp.top).offset(3)
+            $0.leading.equalTo(userImageView.snp.trailing).offset(9)
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(userName.snp.bottom).offset(2)
+            $0.leading.equalTo(userName.snp.leading)
+        }
+        
+        contentLabel.snp.makeConstraints {
+            $0.leading.equalTo(userImageView.snp.leading)
+            $0.trailing.equalTo(contentView.snp.trailing)
+            $0.top.equalTo(userImageView.snp.bottom).offset(15)
+        }
+        
+        likeButton.snp.makeConstraints {
+            $0.top.equalTo(contentLabel.snp.bottom).offset(10)
+            $0.leading.equalTo(contentLabel.snp.leading)
+            $0.height.width.equalTo(25)
+        }
+        
+        bookMarkButton.snp.makeConstraints {
+            $0.top.equalTo(likeButton.snp.top)
+            $0.leading.equalTo(likeButton.snp.trailing).offset(6)
+            $0.width.height.equalTo(25)
+        }
+        
+        likeCountLabel.snp.makeConstraints {
+            $0.leading.equalTo(bookMarkButton.snp.trailing).offset(151)
+            $0.bottom.equalTo(bookMarkButton.snp.bottom)
+        }
+        
+        commentCountLabel.snp.makeConstraints {
+            $0.bottom.equalTo(likeCountLabel.snp.bottom)
+            $0.leading.equalTo(likeCountLabel.snp.trailing).offset(10)
+        }
+        
+        viewsCountLabel.snp.makeConstraints {
+            $0.bottom.equalTo(likeCountLabel.snp.bottom)
+            $0.leading.equalTo(commentCountLabel.snp.trailing).offset(10)
+        }
+    }
+    
+}
---
+++ FINPO/MyPage/SegmentedControl/WriteMySelfViewController.swift
@@ -0,0 +1,69 @@
+//
+//  WriteMySelfViewController.swift
+//  FINPO
+//
+//  Created by 이동희 on 2022/06/14.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+import Kingfisher
+
+final class WriteMySelfViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
+    
+    let dummyItems = Observable.just([
+        " Usage of text input box ",
+        " Usage of switch button ",
+        " Usage of progress bar ",
+        " Usage of text labels ",
+        ])
+    
+    private var boardTableView: UITableView = {
+//        let tv = UITableView(frame: self.view.frame, style: .plain)
+        let tv = UITableView()
+        tv.rowHeight = CGFloat(150)
+        tv.bounces = false
+        return tv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setAttribute()
+        setLayout()
+        setInputBind()
+        setOutputBind()
+    }
+    
+
+    
+    fileprivate func setAttribute() {
+        view.backgroundColor = .white
+        
+        boardTableView.register(BoardTableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    fileprivate func setLayout() {
+        view.addSubview(boardTableView)
+        boardTableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    fileprivate func setInputBind() {
+
+    }
+    
+    fileprivate func setOutputBind() {
+        dummyItems
+            .bind(to: boardTableView.rx.items(cellIdentifier: "cell", cellType: BoardTableViewCell.self)) { (index, element, cell) in
+                
+            }.disposed(by: disposeBag)
+    }
+    
+}
