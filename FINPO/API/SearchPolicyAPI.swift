f989a7ddcd367f14fbb45e77b9973b03b8ee9d14
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-07-04 17:55:49 +0900

    [추가] 정책 상세 API

--- FINPO/API/SearchPolicyAPI.swift
+++ FINPO/API/SearchPolicyAPI.swift
@@ -12,64 +12,74 @@ import RxSwift
 import RxCocoa
 
 struct SearchPolicyAPI {
-//    static func searchPolicyAPI(title: String, completion: @escaping (Result<SearchPolicyResponse, Error>) -> Void) {
-//        let encodedTitle = title.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-//        let accessToken = UserDefaults.standard.string(forKey: "accessToken")!
-//
-//        let url = "https://dev.finpo.kr/policy/search?title=\(encodedTitle)"
-//
-//        let parameter: Parameters = [
-//            "title": encodedTitle
-//        ]
-//
-//        let header: HTTPHeaders = [
-//            "Content-Type": "application/json;charset=UTF-8",
-//            "Authorization": "Bearer ".appending(accessToken)
-//        ]
-//
-//
-//        AF.request(url, method: .get, parameters: parameter, encoding: URLEncoding.default, headers: header)
-//            .validate()
-//            .responseJSON { (response) in
-//                switch response.result {
-//                case .success(let data):
-//                    do {
-//                        let jsonData = try JSONSerialization.data(withJSONObject: data, options: .prettyPrinted)
-//                        let json = try JSONDecoder().decode(SearchPolicyResponse.self, from: jsonData)
-//                        print("정책 조회 제이슨 리스폰스!!!: \(json)")
-//                        completion(.success(json))
-//                    } catch(let err) {
-//                        print(err.localizedDescription)
-//                    }
-//                case .failure(let err):
-//                    print(err.localizedDescription)
-//                    break
-//                }
-//            }
-//    }
     
-    static func searchPolicyAPI(title: String, at page: Int = 0) -> Observable<SearchPolicyResponse> {
+    static func searchPolicyAPI(title: String, at page: Int = 0, to categories: [Int], in region: [Int]) -> Observable<SearchPolicyResponse> {
+
         return Observable.create { observer in
-            if(title == "") {
-                return Disposables.create()
+            
+            let accessToken = UserDefaults.standard.string(forKey: "accessToken") ?? ""
+            
+            let urlStr = "https://dev.finpo.kr/policy/search?size=10&sort=title,asc&sort=modifiedAt,desc"
+            
+            let encodedStr = urlStr.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+
+            let url = URL(string: encodedStr)!
+            
+            //만약 타이틀이 빈값이면 설정한 거주지역을 넣기
+            let parameter: Parameters
+            if title == "" {
+                parameter = [
+                    "region": region,
+                    "category": categories,
+                    "page": page
+                ]
+            } else {
+                parameter = [
+                    "region": region,
+                    "category": categories,
+                    "page": page,
+                    "title": title
+                ]
             }
-            print("들어온 페이지 값: \(page)")
-//            print("들어온 타이틀 값: \(title)")
-            let encodedTitle = title.addingPercentEncoding(withAllowedCharacters: .urlUserAllowed)!
-//            print("인코딩된 타이틀 값: \(encodedTitle)")
+            
+            let header: HTTPHeaders = [
+                "Content-Type": "application/json;charset=UTF-8",
+                "Authorization": "Bearer ".appending(accessToken)
+            ]
+            
+            API.session.request(url, method: .get, parameters: parameter, encoding: URLEncoding(arrayEncoding: .noBrackets), headers: header, interceptor: MyRequestInterceptor())
+                .validate(statusCode: 200..<300)
+                .responseJSON { (response) in
+                    switch response.result {
+                    case .success(let data):
+                        do {
+                            let jsonData = try JSONSerialization.data(withJSONObject: data, options: [])
+                            let json = try JSONDecoder().decode(SearchPolicyResponse.self, from: jsonData)
+                            observer.onNext(json)
+                        } catch(let err) {
+                            print(err.localizedDescription)
+                        }
+                    case .failure(let err):
+                        print(err.localizedDescription)
+                        observer.onError(err)
+                    }
+                }
+            
+            return Disposables.create()
+        }
+    }
+    
+    static func searchPolicyAsPopular(title: String, at page: Int = 0) -> Observable<SearchPolicyResponse> {
+        
+        let user = User.instance
+        
+        return Observable.create { observer in
+//            if(title == "") {
+//                return Disposables.create()
+//            }
             let accessToken = UserDefaults.standard.string(forKey: "accessToken")!
-            
-//            let url = "https://dev.finpo.kr/policy/search?title=\(encodedTitle)&region=107,4,10,11,102,104&category=9,10,11&page=0&size=5&sort=title,asc&sort=modifiedAt,desc"
-            let urlStr = "https://dev.finpo.kr/policy/search?title=\(title)&category=9,10,11&page=\(page)&size=10&sort=title,asc&sort=modifiedAt,desc"
-            
-//            let parameter: Parameters = [
-//                "title": encodedTitle,
-////                "region": [107,4,10,11,102,104],
-//                "category": [9,10,11],
-//                "page": 0,
-//                "size": 5,
-//                "sort": ["title,asc", "modifiedAt,desc"]
-//            ]
+            print("타이틀: \(title), 불러 올 페이지: \(page)")
+            let urlStr = "https://dev.finpo.kr/policy/search?size=10&sort=countOfInterest,desc"
             
             let encodedStr = urlStr.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
 
@@ -79,22 +89,30 @@ struct SearchPolicyAPI {
                 "Content-Type": "application/json;charset=UTF-8",
                 "Authorization": "Bearer ".appending(accessToken)
             ]
-            //%25EB%25B6%2580%25EC%2582%25B0. %EB%B6%80%EC%82%B0
-            AF.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: header)
+            
+            let parameter: Parameters = [
+                "region": user.region,
+                "category": user.category,
+                "page": page,
+                "title": title
+            ]
+            
+            API.session.request(url, method: .get, parameters: parameter, encoding: URLEncoding.default, headers: header, interceptor: MyRequestInterceptor())
                 .validate(statusCode: 200..<300)
                 .responseJSON { (response) in
                     switch response.result {
                     case .success(let data):
                         do {
                             let jsonData = try JSONSerialization.data(withJSONObject: data, options: [])
-//                            let jsonData = try JSONSerialization.jsonObject(with: data, options: [])
                             let json = try JSONDecoder().decode(SearchPolicyResponse.self, from: jsonData)
+                            print("인기순 정렬 API 데이터 리스폰스: \(json)")
                             observer.onNext(json)
                         } catch(let err) {
-                            print(err.localizedDescription)
+                            print("인기순 불러오기 실패!!!")
+                            print(err)
                         }
                     case .failure(let err):
-                        print(err.localizedDescription)
+                        print("인기순 정렬 API 에러 발생: \(err.localizedDescription)")
                         observer.onError(err)
                     }
                 }
