7cc305ff614968c6e99174fe7aeace58ed10d428
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-07-12 20:53:21 +0900

    [추가] 알림 삭제 API

--- FINPO/API/AlarmAPI.swift
+++ FINPO/API/AlarmAPI.swift
@@ -22,17 +22,62 @@ struct AlarmAPI {
                 "Authorization": "Bearer ".appending(accessToken)
             ]
             
-            API.session.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: header, interceptor: MyRequestInterceptor())
-                .responseDecodable(of: AlarmModel.self) { response in
-                    switch response.value {
-                    case .some(let data):
-                        print("가져온 알람 데이터: \(data)")
-                        observer.onNext(data)
-                    case .none:
-                        observer.onCompleted()
+            AF.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: header, interceptor: MyRequestInterceptor())
+                .validate(statusCode: 200..<300)
+                .responseJSON { response in
+                    switch response.result {
+                    case .success(let data):
+                        do {
+                            let jsonData = try JSONSerialization.data(withJSONObject: data, options: [])
+                            let json = try JSONDecoder().decode(AlarmModel.self, from: jsonData)
+                            observer.onNext(json)
+                        } catch(let err) {
+                            print("알림 데이터 가져오기 실패!: \(err)")
+                        }
+                    case .failure(let err):
+                        print("에러 발생!!: \(err)")
+                        observer.onError(err)
                     }
                 }
                 
+                
+            return Disposables.create()
+        }
+    }
+    
+    static func deleteMyAlarm(policyId: Int) -> Observable<Bool> {
+        return Observable.create { observer in
+            
+            let url = BaseURL.url.appending("notification/history/\(policyId)")
+            
+            let accessToken = UserDefaults.standard.string(forKey: "accessToken") ?? ""
+            
+            let header: HTTPHeaders = [
+                "Content-Type": "application/json;charset=UTF-8",
+                "Authorization": "Bearer ".appending(accessToken)
+            ]
+            
+            API.session.request(url, method: .delete, parameters: nil, encoding: URLEncoding.queryString, headers: header, interceptor: MyRequestInterceptor())
+                .validate(statusCode: 200..<600)
+                .responseJSON { response in
+                    switch response.result {
+                    case .success(let data):
+                        do {
+                            let jsonData = try response.result.get() as? [String: Any]
+                            print("제이슨 데이터: \(jsonData)")
+                            let result = jsonData?["success"] as? Bool ?? false
+                            print("알림 삭제 결과: \(result)")
+                            observer.onNext(result)
+                        } catch(let err) {
+                            print("err occured: \(err)")
+                        }
+                        
+                    case .failure(let err):
+                        print("알림 삭제 실패 에러: \(err)")
+                        observer.onError(err)
+                    }
+                }
+            
             return Disposables.create()
         }
     }
