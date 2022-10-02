600b5cf60cb2d11e72de74da1c968f67fd52e281
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-03 01:17:19 +0900

    [추가] 커뮤니티 뷰 바인딩

--- FINPO/Home/Community/CommunitySearchViewController.swift
+++ FINPO/Home/Community/CommunitySearchViewController.swift
@@ -48,11 +48,12 @@ class CommunitySearchViewController: UIViewController {
         return searchBar
     }()
     
-    private var resultTableView: UITableView = {
+    private lazy var resultTableView: UITableView = {
         let resultTableView = UITableView()
         resultTableView.backgroundColor = UIColor.G09
         resultTableView.rowHeight = CGFloat(150)
         resultTableView.separatorInset.left = 0
+        resultTableView.register(BoardTableViewCell.self, forCellReuseIdentifier: "BoardTableViewCell")
         return resultTableView
     }()
     
@@ -72,11 +73,103 @@ class CommunitySearchViewController: UIViewController {
     }
     
     private func setInputBind() {
+        //SearchBar의 검색 버튼 or TableView LoadMore 이벤트 시 불러오기
+//        Observable.merge(
+//            searchBar.rx.searchButtonClicked.asObservable(),
+//            resultTableView.rx.reachedBottom(from: -25).asObservable()
+//        )
         
+        //Tap이 되었을 때 Text를 ViewModel로 보내기
+        searchBar.rx.searchButtonClicked
+            .withLatestFrom(searchBar.rx.text) { $1 ?? "" }
+            .filter { !$0.isEmpty }
+            .distinctUntilChanged()
+            .subscribe(onNext: { [weak self] text in self?.viewModel.contentText.onNext((content: text, page: 0)) })
+            .disposed(by: disposeBag)
+        
+        //테이블 더 불러오기
+        resultTableView.rx.reachedBottom(from: -25)
+            .bind(to: viewModel.increasePage)
+            .disposed(by: disposeBag)
+        
+        //재검색시 page 0
     }
     
     private func setOutputBind() {
-        
+        viewModel.fetchBoard
+            .scan(into: [CommunityContentModel]()) { boards, response in
+                boards.removeAll()
+                for board in response.data.content {
+                    boards.append(board)
+                }
+            }
+            .observe(on: MainScheduler.asyncInstance)
+            .bind(to: resultTableView.rx.items(cellIdentifier: "BoardTableViewCell", cellType: BoardTableViewCell.self)) {
+                (index, element, cell) in
+                cell.selectionStyle = .none
+                if let imageStr = element.user?.profileImg {
+                    let profileImgURL = URL(string: imageStr)
+                    cell.userImageView.kf.setImage(with: profileImgURL)
+                } else {
+                    cell.userImageView.image = UIImage(named: "profile=Default_72")
+                }
+                ///익명글
+                if element.anonymity {
+                    cell.userName.text = "(익명)"
+                } else {
+                    cell.userName.text = element.user?.nickname ?? "(알 수 없음)"
+                }
+                ///Date
+                let format = DateFormatter()
+                format.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+                format.locale = Locale(identifier: "ko")
+                format.timeZone = TimeZone(abbreviation: "KST")
+                var tempDate: Date
+                element.isModified ? (tempDate = format.date(from: element.modifiedAt) ?? Date()) : (tempDate = format.date(from: element.createdAt) ?? Date())
+                format.dateFormat = "yyyy년 MM월 dd일 a hh:mm"
+                format.amSymbol = "오전"
+                format.pmSymbol = "오후"
+                let str = format.string(from: tempDate)
+                cell.dateLabel.text = str
+                cell.contentLabel.text = element.content
+                
+                ///좋아요, 댓글, 북마크 수
+                cell.likeCountLabel.text = "좋아요 \(element.likes)"
+                cell.viewsCountLabel.text = "・ 댓글 \(element.countOfComment)"
+                cell.commentCountLabel.text = "・ 조회수 \(element.hits)"
+                
+                if(element.isLiked) {
+                    cell.likeButton.setImage(UIImage(named: "like_active")?.withRenderingMode(.alwaysOriginal), for: .normal)
+                } else {
+                    cell.likeButton.setImage(UIImage(named: "like"), for: .normal)
+                }
+                
+//                cell.likeButton.rx.tap
+//                    .subscribe(onNext: { _ in
+//                        if(element.isLiked) {
+//                            self.viewModel.input.unlikeObserver.accept(element.id)
+//                        }
+//                        else {
+//                            self.viewModel.input.likeObserver.accept(element.id)
+//                        }
+//                    }).disposed(by: cell.cellBag)
+                
+                if(element.isBookmarked) {
+                    cell.bookMarkButton.setImage(UIImage(named: "scrap_active"), for: .normal)
+                } else {
+                    cell.bookMarkButton.setImage(UIImage(named: "scrap_inactive"), for: .normal)
+                }
+                
+//                cell.bookMarkButton.rx.tap
+//                    .subscribe(onNext: { [weak self] _ in
+//                        if(element.isBookmarked) {
+//                            self?.viewModel.input.undoBookmarkObserver.accept(element.id)
+//                        } else {
+//                            self?.viewModel.input.doBookmarkObserver.accept(element.id)
+//                        }
+//                    }).disposed(by: cell.cellBag)
+            }
+            .disposed(by: disposeBag)
     }
 }
 
