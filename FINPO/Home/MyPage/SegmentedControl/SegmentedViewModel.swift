2c9a58ffe2cca6a8d3375fc784dc8fc785170143
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-10 00:51:17 +0900

    [추가] 마이페이지 게시글 ViewModel

--- FINPO/Home/MyPage/SegmentedControl/SegmentedViewModel.swift
+++ FINPO/Home/MyPage/SegmentedControl/SegmentedViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+protocol SegmentedViewModelType {
+    var fetchMyWriting: AnyObserver<Void> { get }
+    
+    var mywritingResult: Observable<CommunityboardResponseModel> { get }
+}
+
+class SegmentedViewModel: SegmentedViewModelType {
+    let disposeBag = DisposeBag()
+    
+    // INPUT //
+    var fetchMyWriting: AnyObserver<Void>
+
+    // OUTPUT //
+    var mywritingResult: Observable<CommunityboardResponseModel>
+    
+    init(domain: MyPageFetchable = MypageStore()) {
+        let writingFetching = PublishSubject<Void>()
+        let page = BehaviorSubject<Int>(value: 0)
+        
+        let mywriting = PublishSubject<CommunityboardResponseModel>()
+        
+        fetchMyWriting = writingFetching.asObserver()
+        writingFetching.withLatestFrom(page.asObservable())
+            .map { domain.fetchMyWriting($0) }
+            .flatMap { $0 }
+            .bind(to: mywriting)
+            .disposed(by: disposeBag)
+            
+        mywritingResult = mywriting.asObservable()
+    }
+}
