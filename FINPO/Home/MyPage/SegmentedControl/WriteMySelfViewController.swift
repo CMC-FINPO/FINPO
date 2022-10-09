b8c0a78bba0caf9d84c73241752e608fbbe5d295
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-10 00:49:59 +0900

    [추가] 마이페이지 내가 작성한 글 View 및 바인딩

--- FINPO/Home/MyPage/SegmentedControl/WriteMySelfViewController.swift
+++ FINPO/Home/MyPage/SegmentedControl/WriteMySelfViewController.swift
@@ -14,7 +14,18 @@ import Kingfisher
 
 final class WriteMySelfViewController: UIViewController {
     
+    let viewModel: SegmentedViewModelType
     let disposeBag = DisposeBag()
+    
+    init(viewModel: SegmentedViewModelType = SegmentedViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        viewModel = SegmentedViewModel()
+        super.init(coder: coder)
+    }
     
     let dummyItems = Observable.just([
         " Usage of text input box ",
@@ -23,11 +34,13 @@ final class WriteMySelfViewController: UIViewController {
         " Usage of text labels ",
         ])
     
-    private var boardTableView: UITableView = {
-//        let tv = UITableView(frame: self.view.frame, style: .plain)
+    private lazy var boardTableView: UITableView = {
         let tv = UITableView()
         tv.rowHeight = CGFloat(150)
-        tv.bounces = false
+        tv.bounces = true
+        tv.showsVerticalScrollIndicator = false
+        tv.showsHorizontalScrollIndicator = false
+        tv.register(BoardTableViewCell.self, forCellReuseIdentifier: "cell")
         return tv
     }()
     
@@ -43,7 +56,7 @@ final class WriteMySelfViewController: UIViewController {
     fileprivate func setAttribute() {
         view.backgroundColor = .white
         
-        boardTableView.register(BoardTableViewCell.self, forCellReuseIdentifier: "cell")
+
     }
     
     fileprivate func setLayout() {
@@ -54,13 +67,61 @@ final class WriteMySelfViewController: UIViewController {
     }
     
     fileprivate func setInputBind() {
-
+        let firstLoad = rx.viewWillAppear
+            .map { _ in () }
+        
+        let reload = boardTableView.refreshControl?.rx
+            .controlEvent(.valueChanged)
+            .map { _ in () } ?? Observable.just(())
+        
+        Observable.merge([firstLoad, reload])
+            .bind(to: viewModel.fetchMyWriting)
+            .disposed(by: disposeBag)
+            
     }
     
     fileprivate func setOutputBind() {
-        dummyItems
-            .bind(to: boardTableView.rx.items(cellIdentifier: "cell", cellType: BoardTableViewCell.self)) { (index, element, cell) in
+        
+        viewModel.mywritingResult
+            .scan(into: [CommunityContentModel]()) { data, from in
+                data.removeAll()
+                for i in 0..<from.data.content.count {
+                    data.append(from.data.content[i])
+                }
+            }
+            .observe(on: MainScheduler.asyncInstance)
+            .bind(to: boardTableView.rx.items(cellIdentifier: "cell", cellType: BoardTableViewCell.self)) {
+                (index, element, cell) in
+                if let imageStr = element.user?.profileImg {
+                    let profileImgURL = URL(string: imageStr)
+                    cell.userImageView.kf.setImage(with: profileImgURL)
+                } else {
+                    cell.userImageView.image = UIImage(named: "profile=Default_72")
+                }
+                ///익명글
+                if element.anonymity {
+                    cell.userName.text = "(익명)"
+                } else {
+                    cell.userName.text = element.user?.nickname ?? "(알 수 없음)"
+                }
+                ///Date
+                let format = DateFormatter()
+                format.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+                format.locale = Locale(identifier: "ko")
+                format.timeZone = TimeZone(abbreviation: "KST")
+                var tempDate: Date
+                element.isModified ? (tempDate = format.date(from: element.modifiedAt) ?? Date()) : (tempDate = format.date(from: element.createdAt) ?? Date())
+                format.dateFormat = "yyyy년 MM월 dd일 a hh:mm"
+                format.amSymbol = "오전"
+                format.pmSymbol = "오후"
+                let str = format.string(from: tempDate)
+                cell.dateLabel.text = str
+                cell.contentLabel.text = element.content
                 
+                ///좋아요, 댓글, 북마크 수
+                cell.likeCountLabel.text = "좋아요 \(element.likes)"
+                cell.viewsCountLabel.text = "・ 댓글 \(element.countOfComment)"
+                cell.commentCountLabel.text = "・ 조회수 \(element.hits)"
             }.disposed(by: disposeBag)
     }
     
