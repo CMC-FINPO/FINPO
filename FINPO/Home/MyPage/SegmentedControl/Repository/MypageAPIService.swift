7bb3e5e9c9c0c504a191c136b60deb0dbdae5e0c
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-10 00:50:40 +0900

    [추가] 마이페이지 내가 작성한 글 Repository(Network)

--- FINPO/Home/MyPage/SegmentedControl/Repository/MypageAPIService.swift
+++ FINPO/Home/MyPage/SegmentedControl/Repository/MypageAPIService.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import RxSwift
+import Alamofire
+
+class MypageAPIService {
+    static func fetchMywritingRx(_ page: Int) -> Observable<CommunityboardResponseModel> {
+        return Observable.create { observer in
+            fetchMywriting(page) { result in
+                switch result {
+                case .success(let data):
+                    observer.onNext(data)
+                    observer.onCompleted()
+                case .failure(let err):
+                    observer.onError(err)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    static func fetchMywriting(_ page: Int, onComplete: @escaping (Result<CommunityboardResponseModel, Error>) -> Void) {
+        let url = BaseURL.url.appending("post/me?page=\(page)&size=5&sort=id,desc")
+        let header = ApiManager.createHeader(token: KeyChain.read(key: KeyChain.accessToken)!)
+        API.session.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: header, interceptor: MyRequestInterceptor(), requestModifier: nil)
+            .responseDecodable(of: CommunityboardResponseModel.self) { result in
+                switch result.value {
+                case .some(let data):
+                    onComplete(.success(data))
+                case .none:
+                    break
+                }
+            }
+    }
+}
