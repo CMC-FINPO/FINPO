8276f5a831b6c3c54501903119bb7b9a29712588
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-10-13 14:48:45 +0900

    [수정] 마이페이지 도메인 데이터 가공 후 리턴

--- FINPO/Home/MyPage/SegmentedControl/Domain/MypageStore.swift
+++ FINPO/Home/MyPage/SegmentedControl/Domain/MypageStore.swift
@@ -16,14 +16,32 @@ protocol MyPageFetchable {
 
 class MypageStore: MyPageFetchable {
     func fetchMyWriting(_ page: Int) -> Observable<CommunityboardResponseModel> {
-        return MypageAPIService.fetchMywritingRx(page)       
+        return MypageAPIService.fetchMywritingRx(page)
+            .map { data in
+                guard let response = try? JSONDecoder().decode(CommunityboardResponseModel.self, from: data) else {
+                    throw NSError(domain: "Decoding error", code: -1)
+                }
+                return response
+            }
     }
 
     func fetchMyCommenting(_ page: Int) -> Observable<CommunityboardResponseModel> {
         return MypageAPIService.fetchMyCommentingRx(page)
+            .map { data in
+                guard let response = try? JSONDecoder().decode(CommunityboardResponseModel.self, from: data) else {
+                    throw NSError(domain: "Decoding error", code: -1)
+                }
+                return response
+            }
     }
     
     func fetchMyLiking(_ page: Int) -> Observable<CommunityboardResponseModel> {
         return MypageAPIService.fetchMyLikingRx(page)
+            .map { data in
+                guard let response = try? JSONDecoder().decode(CommunityboardResponseModel.self, from: data) else {
+                    throw NSError(domain: "Decoding error", code: -1)
+                }
+                return response
+            }
     }
 }
